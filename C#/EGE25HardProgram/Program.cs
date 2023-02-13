3c1c84725d29145cc7af148f27775e29744c05d2
KiraYagamov <isaru@sasuke.ru>
2023-02-13 16:22:07 +0300

    мда

--- C#/EGE25HardProgram/Program.cs
+++ C#/EGE25HardProgram/Program.cs
@@ -17,8 +17,8 @@ namespace Program
         int maxNum;
         public void MainVoid(){
             for(int i = 125697; i <= 190234; i++){
-                for(int b = 2; b <= i; b++){
-                    if(i%b == 0){
+                for(int b = i-1; b >= 1; b--){
+                    if(i%b == 0 && is_prime(b)){
                         delitels.Add(b);
                     }
                 }
@@ -35,6 +35,14 @@ namespace Program
                 delitels.Clear();
             }
             Console.Write(count.ToString() + " " + maxNum.ToString());
+        }
+        bool is_prime(int x) {
+            for (int i = 2; i <= Math.Pow(x, 0.5f); i++) {
+                if (x % i == 0) {
+                    return false;
+                }
+            }
+            return true;
         }
     }
 }
