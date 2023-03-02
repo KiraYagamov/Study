8d4a18f58d8c1b62342ca6045178d74ccd3aa5a4
KiraYagamov <isaru@sasuke.ru>
2023-03-02 17:25:01 +0300

    ой

--- C#/02.03.23/2/Program.cs
+++ C#/02.03.23/2/Program.cs
@@ -1,7 +1,7 @@
 ﻿int count = 0;
 int F(int n){
     if(n>20){
-        return n*n*n+1;
+        return n*n*n+n;
     }
     else if(n<=20){
         if(n%2==0){
@@ -17,7 +17,10 @@ for(int i = 1; i<=1000; i++){
     for(int b = 0; b<(F(i).ToString()).Length; b++){
         if(int.Parse((F(i).ToString())[b].ToString()) == 1){
             count++;
+            goto Next;
         }
     }
+    Next:
+    count = count;
 }
 Console.Write(1000-count);
