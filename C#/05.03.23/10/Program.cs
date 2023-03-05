f1959080e62f0bcf9b98e10ab31d52eb10daadc5
KiraYagamov <105204832+KiraYagamov@users.noreply.github.com>
2023-03-05 14:25:47 +0300

    домашка

---
+++ C#/05.03.23/10/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+Console.Write("Введите длинну массива: ");
+int count = int.Parse(Console.ReadLine());
+List<int> randMas = new List<int>();
+List<int> trueMas = new List<int>();
+Random rnd = new Random();
+int sum = 0;
+for(int i = 0; i<count;i++){
+    randMas.Add(rnd.Next(0, 1000000000));
+}
+Console.WriteLine("Введите N: ");
+int n = int.Parse(Console.ReadLine());
+for(int i = 0; i<randMas.Count; i++){
+    for(int b = 0; b<(randMas[i].ToString()).Length; b++){
+        sum+=int.Parse((randMas[i].ToString())[b].ToString());
+    }
+    if(sum == n){
+        trueMas.Add(randMas[i]);
+    }
+    sum = 0;
+}
+for(int i = 0; i<trueMas.Count; i++){
+    Console.WriteLine(trueMas[i]);
+}
---
+++ C#/05.03.23/11/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections.Generic;
+Console.Write("Введите длинну массива: ");
+int count = int.Parse(Console.ReadLine());
+List<int> randMas = new List<int>();
+List<int> trueMas = new List<int>();
+Random rnd = new Random();
+int del = 0;
+for(int i = 0; i<count;i++){
+    randMas.Add(rnd.Next(0, 1000000));
+}
+for(int i = 0; i<randMas.Count; i++){
+    for(int b = 1; b<randMas[i]; b++){
+        if(randMas[i]%b == 0){
+            del++;
+        }
+    }
+    if(del==1){
+        trueMas.Add(randMas[i]);
+    }
+    del = 0;
+}
+for(int i = 0; i<trueMas.Count; i++){
+    Console.WriteLine(trueMas[i]);
+}
