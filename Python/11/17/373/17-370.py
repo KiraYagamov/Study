dc91b721b27be34d73f09e7817817069836e2558
KiraYagamov <isaru@sasuke.ru>
2023-09-16 19:08:27 +0300

    Create 17-370.py

---
+++ Python/11/17/373/17-370.py
@@ -0,0 +1,38 @@
+def to_trinary(num):
+    trinary = ""
+    while(num > 0):
+        trinary += str(num%3)
+        num = num//3
+    return trinary[::-1]
+
+file = open('17-370.txt', "r")
+lines = []
+three = []
+minThree = 0
+minSumSqr = 0
+count = 0
+
+for line in file:
+    lines.append(int(line))
+
+for num in lines:
+    if(len(str(abs(num))) == 3):
+        three.append(num)
+three.sort()
+
+for num in three:
+    if(str(num)[-1] == "3"):
+        minThree = num
+        break
+
+for i in range(0, len(lines)-1):
+    if(len(str(abs(lines[i]))) == 4 and len(str(abs(lines[i+1]))) != 4) or (len(str(abs(lines[i]))) != 4 and len(str(abs(lines[i+1]))) == 4):
+        sum = lines[i]**2 + lines[i+1]**2
+        if(sum/minThree == int(sum/minThree)):
+            if(minSumSqr == 0 or minSumSqr > sum):
+                minSumSqr = sum
+            count += 1
+
+print(count, minSumSqr)
+
+#Вывод: 12 19025168
