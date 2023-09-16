cad5e86c28cbafd651a1e2d5e5ed6aa17e8a17fc
KiraYagamov <isaru@sasuke.ru>
2023-09-16 17:55:29 +0300

    sdaf

---
+++ Python/11/17/373/17-373.py
@@ -0,0 +1,37 @@
+def to_trinary(num):
+    trinary = ""
+    while(num > 0):
+        trinary += str(num%3)
+        num = num//3
+    return trinary[::-1]
+
+file = open('17-370.txt', "r")
+lines = []
+three = []
+maxThree = 0
+minSumSqr = 0
+count = 0
+
+for line in file:
+    lines.append(int(line))
+for num in lines:
+    if(num > 99 and num < 1000):
+        three.append(num)
+three.sort()
+
+for i in range(len(three)-1, -1, -1):
+    trinary = to_trinary(three[i])
+    if(trinary == trinary[::-1]):
+        maxThree = three[i]
+        print(maxThree, trinary)
+        break
+
+for i in range(0, len(lines)-1):
+    if(abs(lines[i]) > 999 and abs(lines[i]) < 10000 and (abs(lines[i+1]) < 999 or abs(lines[i+1]) > 10000)) or (abs(lines[i+1]) > 999 and abs(lines[i+1]) < 10000 and (abs(lines[i]) < 999 or abs(lines[i]) > 10000)):
+        sum = lines[i]**2 + lines[i+1]**2
+        if(sum/maxThree == int(sum/maxThree)):
+            if(minSumSqr == 0 or minSumSqr > sum):
+                minSumSqr = sum
+            count += 1
+            
+print(count, minSumSqr)
